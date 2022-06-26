4623865065b49b8f5f57d3485d781aa38e04455c
PinedaVictor <pinedavictor095@gmail.com>
2022-06-26 11:05:41 -0700

    Added vector icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {MainNavigator} from './src/navigation/MainNavigator';
 
 export const App: React.FC = () => {
   return (
-    // <SafeAreaView>
-
-    <MainNavigator />
-    // </SafeAreaView>
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
   );
 };
--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Home} from '../screens/Home';
-import {Profile} from '../screens/Profile';
+import {Home, Profile, TipCalculator} from '../screens';
 
 export const MainNavigator: React.FC = () => {
   const tabNavigation = createBottomTabNavigator();
   const {Navigator, Screen} = tabNavigation;
   return (
-    <NavigationContainer>
-      <Navigator initialRouteName="Home">
-        <Screen name="Home" component={Home} />
-        <Screen name="Profile" component={Profile} />
-      </Navigator>
-    </NavigationContainer>
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          console.log(route);
+          console.log(color);
+          console.log(size);
+          console.log(focused);
+          return <Icon name="rocket" size={30} color="#0000ff" />;
+        },
+      })}>
+      <Screen name="Home" component={Home} />
+      <Screen name="Profile" component={Profile} />
+      <Screen name="TipCalculator" component={TipCalculator} />
+    </Navigator>
   );
 };
